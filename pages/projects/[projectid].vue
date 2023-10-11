4e8f9268de396449ee13d785480c9d6cb61ebf87
DecDuck <declanahofmeyr@gmail.com>
2023-10-12 09:05:51 +1100

    failed loading handling

--- pages/projects/[projectid].vue
+++ pages/projects/[projectid].vue
@@ -131,15 +131,24 @@
                 item.name.split(" ")[0]
               }}</span>
             </NuxtLink>
-            <button
-              v-else
-              :class="[
-                'transition bg-zinc-600 animate-pulse group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold',
-              ]"
-            >
-              <div class="h-6 w-6 shrink-0" aria-hidden="true" />
-              <span class="sr-only">{{ item.name }}</span>
-            </button>
+            <div v-else class="p-3 bg-zinc-800 rounded-md">
+              <svg
+                aria-hidden="true"
+                class="w-6 h-6 text-transparent animate-spin fill-orange-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+            </div>
           </li>
         </ul>
       </nav>
@@ -200,7 +209,7 @@ const partialProject: PartiallyLoadedProject = route.meta
 const project: FullyLoadedProject = partialProject.projectSource.loadProject(
   partialProject.id
 );
-const schemas: Ref<{ [key: string]: Schema }> = ref({});
+const schemas: Ref<{ [key: string]: Schema | null }> = ref({});
 
 // Utilities
 const constructProjectPath = (path: string) => `/projects/${project.id}${path}`;
@@ -225,16 +234,31 @@ async function loadSchemas() {
       notifications.value.findIndex((e) => e.uuid == notificationID),
       1
     );
+  const pushErrorNotification = () =>
+    notifications.value.push({
+      uuid: crypto.randomUUID().toString(),
+      type: NotificationType.TEXT,
+      title: "Failed to download & import schemas",
+      description:
+        "An error occured while trying to import a schema. Check browser console for more details.",
+    });
 
   for (let i = 0; i < project.schemas.length; i++) {
     const schemaUrl = project.schemas[i];
-    const schema = JSON.parse(await $fetch(schemaUrl)) as Schema;
-    schema.title = cleanSchemaName(schema.title);
 
-    // Store by URL for quick lookup later
-    schemas.value[schemaUrl] = schema;
+    try {
+      const schema = JSON.parse(await $fetch(schemaUrl)) as Schema;
+      schema.title = cleanSchemaName(schema.title);
 
-    updateNotificationProgress((i + 1 / project.schemas.length) * 100);
+      // Store by URL for quick lookup later
+      schemas.value[schemaUrl] = schema;
+
+      updateNotificationProgress((i + 1 / project.schemas.length) * 100);
+    } catch (e) {
+      pushErrorNotification();
+      console.error(e);
+      schemas.value[schemaUrl] = null;
+    }
   }
 
   destroyNotification();
@@ -245,8 +269,8 @@ async function loadSchemas() {
   notifications.value.push({
     uuid: successNotificationID,
     type: NotificationType.TEXT,
-    title: "Loaded project successfully",
-    description: "Successfully loaded project!",
+    title: "Loaded project",
+    description: "Loaded project into web builder!",
   });
 
   setTimeout(() => {
@@ -271,9 +295,14 @@ function generateNavigation() {
     },
   ];
 
+
   project.schemas.forEach((url) => {
-    if (schemas.value[url]) {
-      const schema: Schema = schemas.value[url];
+    if (schemas.value[url] !== undefined) {
+      const schema = schemas.value[url];
+      console.log(schema);
+      if (schema == null) {
+        return;
+      }
       base.push({
         name: schema.title,
         path: `/editor/${schema.$id}`,
