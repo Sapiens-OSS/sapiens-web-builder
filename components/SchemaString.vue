e015eed8e9752f3a4f3abeee9883bb3396bb04e9
DecDuck <declanahofmeyr@gmail.com>
2023-10-18 21:15:21 +1100

    added delete options for string

--- components/SchemaString.vue
+++ components/SchemaString.vue
@@ -5,19 +5,29 @@
         calculateSchemaTitle(props.schema)
       }}</ListboxLabel>
       <div class="relative mt-2">
-        <ListboxButton
-          class="relative w-full cursor-default rounded-md bg-zinc-800/40 py-1.5 pl-3 pr-10 text-left text-zinc-200 shadow-sm ring-1 ring-inset ring-zinc-700 focus:outline-none focus:ring-2 focus:ring-orange-600 sm:text-sm sm:leading-6"
+        <div
+          class="relative inline-flex overflow-hidden w-full cursor-default rounded-md bg-zinc-800/40 text-left text-zinc-200 shadow-sm ring-1 ring-inset ring-zinc-700 focus:outline-none focus:ring-2 focus:ring-orange-600 sm:text-sm sm:leading-6"
         >
-          <span :class="[model ? '' : 'text-zinc-400', 'block truncate']">{{ model || "No value selected." }}</span>
-          <span
-            class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
-          >
-            <ChevronUpDownIcon
-              class="h-5 w-5 text-gray-400"
-              aria-hidden="true"
+          <ListboxButton class="py-1.5 pl-3 inline-flex justify-between grow">
+            <span :class="[model ? '' : 'text-zinc-400', 'block truncate']">{{
+              model || "No value selected."
+            }}</span>
+            <span class="pointer-events-none flex items-center pr-2">
+              <ChevronUpDownIcon
+                class="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </ListboxButton>
+
+          <span>
+            <TrashIcon
+              v-if="props.elementConfig?.deleteAction"
+              @click="props.elementConfig?.deleteAction"
+              class="aspect-square cursor-pointer h-9 p-2 bg-red-600"
             />
           </span>
-        </ListboxButton>
+        </div>
 
         <transition
           leave-active-class="transition ease-in duration-100"
@@ -69,9 +79,19 @@
   <div v-else>
     <label
       :for="componentID"
-      class="block text-sm font-medium leading-6 text-zinc-100"
-      >{{ calculateSchemaTitle(props.schema) }}</label
-    >
+      class="inline-flex items-center gap-x-2 text-sm font-medium leading-6 text-zinc-100"
+      >{{ calculateSchemaTitle(props.schema) }}
+
+      <button
+        v-if="props.elementConfig?.deleteAction"
+        @click="props.elementConfig?.deleteAction"
+        type="button"
+        class="inline-flex items-center gap-x-1.5 rounded-md bg-red-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+      >
+        Delete
+        <TrashIcon class="-mr-0.5 h-5 w-5" aria-hidden="true" />
+      </button>
+    </label>
     <div class="mt-2">
       <input
         type="text"
@@ -98,6 +118,7 @@ import {
   ListboxOptions,
 } from "@headlessui/vue";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/vue/20/solid";
+import { TrashIcon } from "@heroicons/vue/24/outline";
 import calculateSchemaTitle from "~/scripts/utils/calculateSchemaTitle";
 
 const props = defineProps<{
